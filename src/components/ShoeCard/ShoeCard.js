f3e3675cf640f9e73ea7d60e4f54f29b6f3e5c1f
Roman Schmocker <roman.schmocker@ergon.ch>
2024-01-04 12:25:28 +0100

    Exercise 4b - strikethrough prices

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -1,53 +1,54 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
-import { formatPrice, pluralize, isNewShoe } from '../../utils';
+import {COLORS, WEIGHTS} from '../../constants';
+import {formatPrice, isNewShoe, pluralize} from '../../utils';
 import Spacer from '../Spacer';
 
 const ShoeCard = ({
-  slug,
-  name,
-  imageSrc,
-  price,
-  salePrice,
-  releaseDate,
-  numOfColors,
-}) => {
-  // There are 3 variants possible, based on the props:
-  //   - new-release
-  //   - on-sale
-  //   - default
-  //
-  // Any shoe released in the last month will be considered
-  // `new-release`. Any shoe with a `salePrice` will be
-  // on-sale. In theory, it is possible for a shoe to be
-  // both on-sale and new-release, but in this case, `on-sale`
-  // will triumph and be the variant used.
-  // prettier-ignore
-  const variant = typeof salePrice === 'number'
-    ? 'on-sale'
-    : isNewShoe(releaseDate)
-      ? 'new-release'
-      : 'default'
+                      slug,
+                      name,
+                      imageSrc,
+                      price,
+                      salePrice,
+                      releaseDate,
+                      numOfColors,
+                  }) => {
+    // There are 3 variants possible, based on the props:
+    //   - new-release
+    //   - on-sale
+    //   - default
+    //
+    // Any shoe released in the last month will be considered
+    // `new-release`. Any shoe with a `salePrice` will be
+    // on-sale. In theory, it is possible for a shoe to be
+    // both on-sale and new-release, but in this case, `on-sale`
+    // will triumph and be the variant used.
+    // prettier-ignore
+    const variant = typeof salePrice === 'number'
+        ? 'on-sale'
+        : isNewShoe(releaseDate)
+            ? 'new-release'
+            : 'default'
 
-  return (
-    <Link href={`/shoe/${slug}`}>
-      <Wrapper>
-        <ImageWrapper>
-          <Image alt="" src={imageSrc} />
-        </ImageWrapper>
-        <Spacer size={12} />
-        <Row>
-          <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
-        </Row>
-        <Row>
-          <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
-        </Row>
-      </Wrapper>
-    </Link>
-  );
+    return (
+        <Link href={`/shoe/${slug}`}>
+            <Wrapper>
+                <ImageWrapper>
+                    <Image alt="" src={imageSrc}/>
+                </ImageWrapper>
+                <Spacer size={12}/>
+                <Row>
+                    <Name>{name}</Name>
+                    <Price strike={!!salePrice}>{formatPrice(price)}</Price>
+                </Row>
+                <Row>
+                    <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+                    <SalePrice>{salePrice && formatPrice(salePrice)}</SalePrice>
+                </Row>
+            </Wrapper>
+        </Link>
+    );
 };
 
 const Link = styled.a`
@@ -81,7 +82,10 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  color: ${({strike}) => strike ? COLORS.gray["700"] : ""};
+  text-decoration: ${({strike}) => strike ? 'line-through' : ""};
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
