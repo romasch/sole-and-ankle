b00e498caa84bcda4be7d75f62f7511219dacd84
Roman Schmocker <roman.schmocker@ergon.ch>
2024-01-04 12:10:39 +0100

    Exercise 4a - shoe grid

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -53,6 +53,8 @@ const ShoeCard = ({
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  flex: 1 1 300px;
+
 `;
 
 const Wrapper = styled.article``;
@@ -61,9 +63,16 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  display: block;
+  height: 100%;
+  width: 100%;
+`;
 
 const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
   font-size: 1rem;
 `;
 
