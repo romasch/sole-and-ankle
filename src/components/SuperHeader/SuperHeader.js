8a04bef488a08a020784e67c482b325ee2d4275c
Roman Schmocker <roman.schmocker@ergon.ch>
2024-01-04 11:00:35 +0100

    Exercise 1 - SuperHeader

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -23,12 +23,19 @@ const SuperHeader = () => {
 };
 
 const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  padding: 8px 32px;
+  
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
 `;
 
 const MarketingMessage = styled.span`
+  flex: 1;
+  
   color: ${COLORS.white};
 `;
 
